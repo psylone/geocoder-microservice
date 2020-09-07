11518e15ea7f6c76bcc13ed308b98ae6c3fe7ed8
Evgeniy Fateev <spaceflow@gmail.com>
2020-09-07 04:31:46 +0300

    feat(mon): add geocoding requests total metrics

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -9,8 +9,11 @@ queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
   Application.logger.info('geocoded coordinates', city: payload['city'], coordinates: coordinates)
 
   if coordinates.present?
+    Metrics.geocoding_requests_total.increment(labels: { result: 'success' })
     client = AdsService::RpcClient.fetch
     client.update_coordinates(payload['id'], coordinates)
+  else
+    Metrics.geocoding_requests_total.increment(labels: { result: 'failure' })
   end
 
   channel.ack(delivery_info.delivery_tag)
--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,1 +1,10 @@
+require 'prometheus/client'
 require 'prometheus/middleware/exporter'
+
+Metrics.configure do |registry|
+  registry.counter(
+    :geocoding_requests_total,
+    docstring: 'The total number of geocoding requests.',
+    labels: %i[result]
+  )
+end
