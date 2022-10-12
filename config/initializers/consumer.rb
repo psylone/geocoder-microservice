35f37cfb183c32570cfd3394f178d06e02148335
safrio <safrio@gmail.com>
2022-10-12 11:53:38 +0500

    remove extra

--- config/initializers/consumer.rb
+++ config/initializers/consumer.rb
@@ -3,14 +3,12 @@ queue = channel.queue('geocoding', durable: true)
 
 queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
   payload = JSON(payload)
-  p "payload: #{payload}"
   coordinates = Geocoder.geocode(payload['city'])
-  p "coordinates: #{coordinates}"
 
   if coordinates.present?
     client = GeocoderService::Client.new
     client.update_coordinates(payload['id'], coordinates)
   end
-
+ensure
   channel.ack(delivery_info.delivery_tag)
 end
